8a061f35c5141dc409084034caa2d80bb6adc37e
JolizbethBonilla <jolizbeth98@gmail.com>
2021-03-26 20:02:36 -0400

    fix cors connection error

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,46 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
 
- class App extends React.Component {
+class App extends React.Component {
   state = {
-    data: null
+    data: null,
   };
 
   componentDidMount() {
-      // Call our fetch function below once the component mounts
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
+    // Call our fetch function below once the component mounts
+    this.callBackendAPI();
+    // .then((res) => this.setState({ data: res.express }))
+    // .catch((err) => console.log(err));
   }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/starter_route');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message) 
-    }
-    return body;
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = () => {
+    fetch("http://localhost:3000/")
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const user = data.username;
+        this.setState({ data: user });
+        return user;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
-  
-  render(){
+
+  // callBackendAPI = async () => {
+  //   const response = await fetch("/starter_route");
+  //   const body = await response.json();
+
+  //   if (response.status !== 200) {
+  //     throw Error(body.message);
+  //   }
+  //   return body;
+  // };
+
+  render() {
     return (
       <div className="App">
         <p className="App-intro">{this.state.data}</p>
--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const queries = require("./db/dbqueries");
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.get("/starter_route", (req, res) => {
   res.send({ express: "The backend is connected to React" });
 });
 
 app.get("/", (req, res) => {
-  //   queries.getAll("users").then((users) => {
-  //     res.json(users);
-  //   });
-  queries.getAll("photos").then((photo) => {
-    res.json(photo);
+  queries.getAll("users").then((users) => {
+    res.json(users);
   });
+  //   queries.getAll("photos").then((photo) => {
+  //     res.json(photo);
+  //   });
   //res.send({ express: "The backend is connected to React" });
 });
 
 app.listen(PORT, () => console.log(`Listen on port http://localhost:${PORT}`));
+module.exports = app;
