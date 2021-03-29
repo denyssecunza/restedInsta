7c7399827b2c7c75a20d209d37c13fdd8a30f891
JolizbethBonilla <jolizbeth98@gmail.com>
2021-03-28 21:40:38 -0400

    add comments fetch

--- client/src/Components/Comments.js
+++ client/src/Components/Comments.js
@@ -3,13 +3,14 @@ import React from "react";
 function Comments(props) {
   const clickedPhoto = props.match.params.photoId;
   const username = props.match.params.username;
-  console.log(username);
   const photoLink = props.location.pathname.split("/");
   photoLink.splice(0, 4);
   const link = photoLink.join("/");
+
   const [comment, setComment] = React.useState([]);
   const [user, setUser] = React.useState([]);
   const [state, setState] = React.useState(0);
+  const [commented, setCommented] = React.useState("");
   React.useEffect(() => {
     let isCurrent = true;
     fetch(`http://localhost:3000/comments`)
@@ -64,7 +65,40 @@ function Comments(props) {
       });
     });
   };
+  console.log(allCommentsbyPhoto);
   photoComments();
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setCommented(e.target.value);
+    // setCommented({ [e.target.username]: e.target.value })
+  };
+  const pho = parseInt(clickedPhoto);
+  const handleSubmitComment = (e) => {
+    e.preventDefault();
+    //  addToComments = (user, body) => {
+    fetch(`http://localhost:3000/comments`, {
+      method: "POST",
+      headers: {
+        Accept: "Application/Json",
+        "Content-Type": "Application/Json",
+      },
+      body: JSON.stringify("Hello I am here "),
+    })
+      .then((response) => console.log(response.json()))
+      .catch((err) => {
+        console.log(err);
+      });
+    // {
+    //   photoId: pho,
+    //   userId: user.id,
+    //   comments_text: commented,
+    // }
+    // setCommented({ comments: [...this.state.comments, { 'username': user, 'body': body }] })
+    // }
+    // setCommented({});
+  };
+
   return (
     <div style={{ paddingTop: "5%", paddingBottom: "5%" }}>
       <div className="container col-4 bg-light">
@@ -91,12 +125,17 @@ function Comments(props) {
           className="d-flex justify-content-center"
           style={{ paddingTop: "5%", paddingBottom: "5%" }}
         >
-          <form className="d-flex justify-content-center">
+          <form
+            className="d-flex justify-content-center"
+            onSubmit={handleSubmitComment}
+          >
             <input
               className="input-group"
               type="text"
               name="comment"
               placeholder="Add a comment..."
+              value={commented}
+              onChange={handleChange}
             />
             <button className="btn btn-light" type="submit">
               {" "}
